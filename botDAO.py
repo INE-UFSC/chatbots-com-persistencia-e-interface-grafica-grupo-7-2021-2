ac48a2089f193c95a1f6c31a02cc4d476b21899d
Luiz Maurício <2003lmvp@gmail.com>
2022-02-15 11:17:36 -0300

    Criado botDAO

---
+++ botDAO.py
@@ -0,0 +1,21 @@
+from DAO import DAO
+from Bots.BotFeliz import BotFeliz
+from Bots.BotTriste import BotTriste
+from Bots.BotZangado import BotZangado
+from Bots.Bot import Bot
+
+class botDAO(DAO):
+    def __init__(self):
+        super().__init__('bots.pkl')
+    
+    def add(self, bot: Bot):
+        if isinstance(bot, Bot):
+            super().add(bot.nome, bot.comandos)
+    
+    def get(self, nome: str):
+        if isinstance(nome, str):
+            super().get(nome)
+    
+    def remove(self, nome: str):
+        if isinstance(nome, str):
+            return super().remove(nome)
